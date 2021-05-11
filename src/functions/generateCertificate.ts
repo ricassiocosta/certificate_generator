49c53ac855ded15e375578d0bab596c92980ef75
ricassiocosta <ricassio_costa@outlook.com>
2021-05-11 17:34:38 -0300

    generating certificate PDF

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,31 @@
+import chromium from 'chrome-aws-lambda'
+import path from 'path'
+import fs from 'fs'
+import handlebars from 'handlebars'
+import dayjs from 'dayjs'
+
 import { document } from '../utils/dynamodbClient'
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+};
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+};
+
+const compile = async function (data: ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -17,6 +39,40 @@ export const handle = async (event) => {
       grade
     }
   }).promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "medal.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    id,
+    name,
+    grade,
+    medal,
+    date: dayjs().format("DD/MM/YYYY"),
+  };
+
+  const content = await compile(data);
+
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
+
+  await browser.close()
 
   return {
     statusCode: 201,
